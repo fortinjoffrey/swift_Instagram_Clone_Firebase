83a7cdd0e6fcd946fc7eab98be1d562573eab314
Joffrey Fortin <fortinjoffrey@gmail.com>
2020-07-29 10:32:11 +0200

    Fix image duplication when loading images
    - Refactor URLSession dataTask calls
      --> Create CustomImageView class containing loadImage func

--- InstagramCloneUsingFirebase/UserProfile/UserProfileHeader.swift
+++ InstagramCloneUsingFirebase/UserProfile/UserProfileHeader.swift
@@ -14,13 +14,15 @@ class UserProfileHeader: UICollectionViewCell {
     
     var user: User? {
         didSet {
-            setupProfileImage()
+            guard let profileImageUrl = user?.profileImageUrl else { return }
+            profileImageView.loadImage(urlString: profileImageUrl)
+
             usernameLabel.text = user?.username
         }
     }
         
-    let profileImageView: UIImageView = {
-        let iv = UIImageView()
+    let profileImageView: CustomImageView = {
+        let iv = CustomImageView()
         iv.layer.cornerRadius = 80 / 2
         iv.layer.masksToBounds = true
         return iv
@@ -119,26 +121,6 @@ class UserProfileHeader: UICollectionViewCell {
         
     }
     
-    fileprivate func setupProfileImage() {
-        
-        guard let profileImageUrl = user?.profileImageUrl else { return }
-        guard let url = URL(string: profileImageUrl) else { return }
-
-        URLSession.shared.dataTask(with: url) { (data, response, err) in
-            
-            if let err = err {
-                print("Failed to fetch profile image : ", err)
-                return
-            }
-            
-            guard let data = data else { return }
-            let profileImage = UIImage(data: data)
-            
-            DispatchQueue.main.async {
-                self.profileImageView.image = profileImage
-            }
-        }.resume()
-    }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
---
+++ InstagramCloneUsingFirebase/Utils/CustomImageView.swift
@@ -0,0 +1,40 @@
+//
+//  CustomImageView.swift
+//  InstagramCloneUsingFirebase
+//
+//  Created by Joffrey Fortin on 29/07/2020.
+//  Copyright © 2020 Joffrey Fortin. All rights reserved.
+//
+
+import UIKit
+
+class CustomImageView: UIImageView {
+    
+    var lastUrlUsedToLoadImage: String?
+    
+    func loadImage(urlString: String) {
+        lastUrlUsedToLoadImage = urlString
+        
+        guard let url = URL(string: urlString) else { return }
+        
+        URLSession.shared.dataTask(with: url) { (data, response, err) in
+            if let err = err {
+                print("Failed to fetch post image: ", err)
+                return
+            }
+            
+            if url.absoluteString != self.lastUrlUsedToLoadImage {
+                return
+            }
+            
+            guard let imageData = data else { return }
+            
+            let photoImage = UIImage(data: imageData)
+            
+            DispatchQueue.main.async {
+                self.image = photoImage
+            }
+            
+        }.resume()
+    }
+}
