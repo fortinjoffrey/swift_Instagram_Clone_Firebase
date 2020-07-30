28728c8bd298a8bdc45bde0a7bce83b9e346de87
Joffrey Fortin <fortinjoffrey@gmail.com>
2020-07-30 15:17:02 +0200

    Refresh home feed when dragged down
    - Implement UIRefreshControl
    - Fetch user posts + following user posts (fetched only the latter before)

--- InstagramCloneUsingFirebase/Home/HomeController.swift
+++ InstagramCloneUsingFirebase/Home/HomeController.swift
@@ -17,12 +17,27 @@ class HomeController: UICollectionViewController, UICollectionViewDelegateFlowLa
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         collectionView.backgroundColor = .white
         
         collectionView.register(HomeFeedCell.self, forCellWithReuseIdentifier: cellId)
         
+        let refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: #selector(handleRefresh), for: .valueChanged)
+        collectionView.refreshControl = refreshControl
         setupNavigationItems()
         
+        fetchAllPosts()
+    }
+    
+    @objc fileprivate func handleRefresh() {
+        print("Handling refresh")
+        posts.removeAll()
+        fetchAllPosts()
+    }
+    
+    fileprivate func fetchAllPosts() {
+        fetchUserPosts()
         fetchFollowingUserPosts()
     }
     
@@ -44,10 +59,20 @@ class HomeController: UICollectionViewController, UICollectionViewDelegateFlowLa
         }
     }
     
+    fileprivate func fetchUserPosts() {
+        guard let uid = Auth.auth().currentUser?.uid else { return }
+        
+        Database.fetchUserWithUID(uid: uid) { (user) in
+            self.fetchPostsWithUser(user: user)
+        }
+    }
+    
     fileprivate func fetchPostsWithUser(user: User) {
 
         let ref = Database.database().reference().child("posts").child(user.uid)
         ref.observeSingleEvent(of: .value, with: { (snapshot) in
+            
+            self.collectionView.refreshControl?.endRefreshing()
             
             guard let dictionaries = snapshot.value as? [String:Any] else { return }
             
