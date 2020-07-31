df0f81dbeae4d9b866a18371239a014fe027d926
Joffrey Fortin <fortinjoffrey@gmail.com>
2020-07-31 10:58:54 +0200

    Show and animate message when save is success

--- InstagramCloneUsingFirebase/Camera/PreviewPhotoContainerView.swift
+++ InstagramCloneUsingFirebase/Camera/PreviewPhotoContainerView.swift
@@ -50,6 +50,38 @@ class PreviewPhotoContainerView: UIView {
             }
             
             print("Successfully saved image to photo library")
+            
+            DispatchQueue.main.async {
+                let savedLabel = UILabel()
+                savedLabel.text = "Saved successfully"
+                savedLabel.textColor = .white
+                savedLabel.textAlignment = .center
+                savedLabel.font = UIFont.boldSystemFont(ofSize: 18)
+                savedLabel.numberOfLines = 0
+                savedLabel.backgroundColor = UIColor(white: 0, alpha: 0.3)
+                
+                self.addSubview(savedLabel)
+                savedLabel.frame = CGRect(x: 0, y: 0, width: 150, height: 80)
+                savedLabel.center = self.center
+                
+                savedLabel.layer.transform = CATransform3DMakeScale(0, 0, 0)
+                
+                UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: .curveEaseOut, animations: {
+                    
+                    savedLabel.layer.transform = CATransform3DMakeScale(1, 1, 1)
+                    
+                }) { (completed) in
+                    
+                    UIView.animate(withDuration: 0.5, delay: 0.75, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: .curveEaseOut, animations: {
+                        
+                        savedLabel.layer.transform = CATransform3DMakeScale(0.1, 0.1, 0.1)
+                        savedLabel.alpha = 0
+                        
+                    }) { (_) in
+                        savedLabel.removeFromSuperview()
+                    }
+                }
+            }
         }
     }
     
